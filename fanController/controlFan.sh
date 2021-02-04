7293e2a2062ceb20b93128d269c93b74b6d40930
ttjsun <sune.jakobsson@telenor.com>
2021-02-04 21:28:11 +0100

    Updates to temperature

--- fanController/controlFan.sh
+++ fanController/controlFan.sh
@@ -16,31 +16,32 @@ if [ -z "$3" ]; then
    echo "Provide a stop time (hh:mm)"
    exit 1
 fi
-limit=$1;
-startTime=$(date -d $2 '+%s');
-stopTime=$(date -d $3 '+%s');
+limit=$(echo "$1 * 1000" | bc );
+startTime=$(date -d "$2" '+%s');
+stopTime=$(date -d "$3" '+%s');
 currentTime=$(date '+%s');
 #
 # check time window
 #
 if [ "$currentTime" -gt "$startTime" ] && [ "$currentTime" -lt "$stopTime" ]; then
-#
-# assuming one wire sensor
-#
-sensorDir="/sys/bus/w1/devices";
-sensorName=$(ls $sensorDir | grep 28);
-#
-# check if sensor is present
-#
-if [ -d "$sensorDir"/"${sensorName}" ]; then
-#
-# get current temperature
-#
-temperature=$(cat "$sensorDir"/"$sensorName"/temperature);
-echo Current temperature is: $temperature
-#
-fi
-
+  #
+  # assuming one wire sensor
+  #
+  sensorDir="/sys/bus/w1/devices";
+  sensorName=$(ls $sensorDir | grep 28);
+  #
+  # check if sensor is present
+  #
+  if [ -d "$sensorDir"/"${sensorName}" ]; then
+    #
+    # get current temperature
+    #
+    temperature=$(cat "$sensorDir"/"$sensorName"/temperature);
+    #
+    if [ "$temperature" -gt "$limit" ]; then
+    echo Run Fan
+    fi
+  fi
 fi
 
 
