1e0170b7e278a5087f19afe18270aeec2f9f98a0
ttjsun <sune.jakobsson@telenor.com>
2021-02-04 07:17:03 +0100

    Updates

--- windowMasterController/make_json.sh
+++ windowMasterController/make_json.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 #
+name="matbod"
+#
 # read io pin, and if it failed set it to -100
 #
-io=$(gpio read 4) || io=-100;
+io=$(gpio read 4) || io=-127;
 #
 # address to one-wire sensor
 #
-sensor=/sys/bus/w1/devices/28-3c01d6075f4c
+sensor="/sys/bus/w1/devices/28-*"
 #
 # check if sensor is there
 #
-if [ -d ${sensor} ]; then
+if [ -d "${sensor}" ]; then
+  #
+  # find the sensor name
+  sensorName=$(ls "$sensor");
 #
 # convert temperature to a decimal number, with three decimals and a preceding zero if needed.
 #
-temperature=$(echo "scale=3; $(cat $sensor/temperature)/1000" | bc -l | awk '{printf "%.3f\n", $0}')
+temperature=$(echo "scale=3; $(cat "$sensor"/temperature)/1000" | bc -l | awk '{printf "%.2f\n", $0}')
 #
-echo "{\"date\":\"$(date)\", \"temperature\": $temperature, \"fan\": $io, \"name\":\"matbod\" }"
+echo "{\"date\":\"$(date)\", \"sensor\":\"$sensorName\", \"temperature\": $temperature, \"fan\": $io, \"name\":\"$name\" }"
 #
 else
 #
-echo "{\"date\":\"$(date)\", \"temperature\": -100, \"fan\": $io, \"name\":\"matbod\" }"
+echo "{\"date\":\"$(date)\", \"temperature\": -127, \"fan\": $io, \"name\":\"$name\" }"
 #
 fi
