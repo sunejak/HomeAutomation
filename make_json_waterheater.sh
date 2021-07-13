b16c144a3a14a959ad13ef12862dc463637431b9
ttjsun <sune.jakobsson@telenor.com>
2021-07-13 16:51:25 +0200

    Cleanup

--- make_json_waterheater.sh
+++ make_json_waterheater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-name="matbod"
+name="water heater"
 #
 # read io pin, and if it failed set it to -100
 #
@@ -14,16 +14,16 @@ sensorError=$?
 #
 # check if sensor is there
 #
-if [ -d "$sensorDir"/"${sensorName}" ] && [ "$sensorError" -eq 0 ] ; then
+if [ -d "$sensorDir"/"${sensorName}" ] && [ "$sensorError" -eq 0 ]; then
 #
 # convert temperature to a decimal number, with three decimals and a preceding zero if needed.
 #
 temperature=$(echo "scale=3; $(cat "$sensorDir"/"$sensorName"/temperature)/1000" | bc -l | awk '{printf "%.2f\n", $0}')
 #
-echo "{\"date\":\"$(date)\", \"sensor\":\"$sensorName\", \"temperature\": $temperature, \"fan\": $io, \"name\":\"$name\" }"
+echo "{\"date\":\"$(date)\", \"sensor\":\"$sensorName\", \"temperature\": $temperature, \"motorA\": $(gpio read 28), \"motorB\": $(gpio read 29), \"name\":\"$name\" }"
 #
 else
 #
-echo "{\"date\":\"$(date)\", \"temperature\": -127, \"fan\": $io, \"name\":\"$name\" }"
+echo "{\"date\":\"$(date)\", \"temperature\": -100, \"motorA\": $(gpio read 28), \"motorB\": $(gpio read 29), \"name\":\"$name\" }"
 #
 fi
