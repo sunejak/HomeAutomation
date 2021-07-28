76b9def8b66feab06a3d253f266b9011fa17e9be
ttjsun <sune.jakobsson@telenor.com>
2021-07-28 12:51:39 +0200

    Fix device list

--- make_device_list.sh
+++ make_device_list.sh
@@ -5,7 +5,7 @@
 # 192.168.1.232 - - [28/Jul/2021:07:00:02 +0200] "GET /dummy.txt?device=garage HTTP/1.1" 200 234 "-" "curl/7.64.0"
 # pick out IP address and name
 #
-device= $(grep dummy /var/log/apache2/access.log | cut -d' ' -f1,7 | sort -n | uniq | sed "s#/dummy.txt?device=##");
+device=$(grep dummy /var/log/apache2/access.log | cut -d' ' -f1,7 | sort -n | uniq | sed "s#/dummy.txt?device=##");
 #
 echo '[';
 ## declare an array variable
@@ -13,9 +13,9 @@ declare -a array=($device)
 # get length of an array
 arrayLength=${#array[@]}
 # use for loop to read all values and indexes
-for (( i=0; i<${arrayLength}; i++ ));
+for (( i=0; i<${arrayLength}; i=i+2 ));
 do
-  echo "index: $i, value: ${array[$i]}"
+  echo "\"IP\": \"${array[$i]}\" , \"name\": \"${array[$i+1]}\", "
 done
 #
 echo ']';
