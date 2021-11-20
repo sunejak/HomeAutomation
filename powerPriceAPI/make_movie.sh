69c84b22d3f980717fce756358d9272f2908228d
ttjsun <sune.jakobsson@telenor.com>
2021-11-20 20:45:04 +0100

    Changed picture rate

--- powerPriceAPI/make_movie.sh
+++ powerPriceAPI/make_movie.sh
@@ -5,9 +5,10 @@
 cd /var/www/html/desk-cam/`date +%m`_`LC_TIME="en_US.UTF-8" date +%b`_`date +%Y`/`date +%d`
 rm amovie.mp4
 #
-# make a 6 minutes video
+# make a 6 minutes video using minute pictures, use framerate 4
+# make a 3 minutes video using 30 second pictures, use framerate 16
 #
-cat cam0_*.jpeg | ffmpeg -framerate 4 -f image2pipe -i - -c:v libx264  -pix_fmt yuv420p amovie.mp4
+cat cam0_*.jpeg | ffmpeg -framerate 16 -f image2pipe -i - -c:v libx264  -pix_fmt yuv420p amovie.mp4
 #
 echo Done at $(date)
 
