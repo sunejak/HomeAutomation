fc28bddc5a609a5fc7a6d9b4d385071b617d9afe
ttjsun <sune.jakobsson@telenor.com>
2021-11-26 13:30:36 +0100

    improved use of jq

--- powerPriceAPI/create_offpeak_time.sh
+++ powerPriceAPI/create_offpeak_time.sh
@@ -14,34 +14,37 @@ fi
 #
 # Get the location for data
 #
-location=$(jq .data.Rows[0].Columns[3].Name "$1" | tr -d '"' )
+location=$(jq -r .data.Rows[0].Columns[3].Name "$1" )
 #
 if [ "$location" != "Tr.heim" ]; then
   echo "No data for Trondheim" "$location"
   exit 1
 fi
+data=$(jq .data "$1");
 #
 # Get the date for the data
 #
-today=$(jq .data.Rows[0].StartTime "$1" | tr -d '"' | cut -c-10 )
+today=$(echo $data | jq -r .Rows[0].StartTime | cut -c-10 )
 #
 # Then pick out the value pr hour and convert NOK/MWh to øre/kWh and add 25% tax
 #
 rm plotdata-"$today".in
 dataset=$(for i in {0..23}; do
-  war=$(jq .data.Rows[$i].Columns[3].Value $1 | tr -d '"' | tr ',' '.' | tr -d ' ')
+  war=$(echo $data | jq -r .Rows[$i].Columns[3].Value  |  tr ',' '.' | tr -d ' ')
   war10=$(echo "scale=2; $war*1.25/10" | bc)
   echo $i $war10 >> plotdata-"$today".in  ;
   done )
 #
 # find lowest price hour
 #
-# low_price_hour=$(for i in {0..23}; do echo $(jq .data.Rows[$i].Columns[3].Value $1) $i ; done | tr -d '"' | sort -n | head -1 | cut -d' '  -f2)
+# low_price_hour=$(for i in {0..23}; do echo $(echo $data | jq -r .data.Rows[$i].Columns[3].Value ) $i ; done | sort -n | head -1 | cut -d' '  -f2)
 #
 # find highest price hour
 #
-high_price_hour=$(for i in {0..23}; do echo $(jq .data.Rows[$i].Columns[3].Value $1) $i ; done | tr -d '"' | tr -d ' ' | sort -n | tail -1 | cut -d' '  -f2)
-high_price=$(jq .data.Rows[$high_price_hour].Columns[3].Value $1 | tr -d '"' | tr -d ' ')
+high_price_hour=$(for i in {0..23}; do echo $(echo $data | jq  -r .Rows[$i].Columns[3].Value ) $i ; done | sort -n | tail -1 | cut -d ' '  -f2)
+# echo Høy time: $high_price_hour;
+high_price=$(echo $data | jq -r .Rows[$high_price_hour].Columns[3].Value  | tr -d ' ')
+# echo Høy pris: $high_price;
 #
 # calculate delta in seconds
 #
@@ -53,6 +56,5 @@ my_time=$(date -d $today '+%s')
 new_time=$(( my_time + delta ))
 #
 window=$(date -d @$new_time)
-#
 #
 echo $new_time $window $location $high_price_hour $high_price $today
