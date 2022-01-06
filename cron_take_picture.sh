a5c6c15e1b60924fc41571558d1f5e34aec15051
Sunejak <sune.jakobsson@gmail.com>
2022-01-06 14:16:56 +0100

    Corrected file name when taking picture

--- cron_take_picture.sh
+++ cron_take_picture.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+  echo "Provide a delay time"
+  exit 1
+fi
+delayTime=$1
 #
 # read io pin, and if it failed set it to -100
 #
@@ -6,13 +11,14 @@ gpio mode 29 out
 gpio mode 28 out
 gpio mode 27 in
 #
-sleep $1
+sleep ${delayTime}
+fileName=outdoor_$(date +%Y)_$(date +%b)_$(date +%d)_$(date +%H)$(date +%M)_$(date +%S).jpeg
 #
 io=$(gpio read 27) || io=-127;
 if [ $io -eq 1 ]; then
-   raspistill -n -o /mnt/ramdisk/outdoor_$(date +%Y)_$(date +%b)_$(date +%d)_$(date +%H)$(date +%M)_$(date +%S).jpeg
+   raspistill -n -o /mnt/ramdisk/outdoor_${fileName}
    echo took picture $(date)
-   scp /mnt/ramdisk/outdoor_$(date +%Y)_$(date +%b)_$(date +%d)_$(date +%H)$(date +%M)_$(date +%S).jpeg  ttjsun@192.168.1.23:/var/www/html/outdoor/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`_${delay}.jpeg
+   scp /mnt/ramdisk/outdoor_${fileName}  ttjsun@192.168.1.23:/var/www/html/outdoor/$(date +%m)_$(date +%b)_$(date +%Y)/$(date +%d)/outdoor_${fileName}.jpeg
    status=$1
    echo $status
 
