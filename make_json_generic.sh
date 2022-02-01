53b7644092b20ad182567db41093de32fb0158cf
Sunejak <sune.jakobsson@gmail.com>
2022-02-01 13:54:09 +0100

    Provide previous temperature as well

--- make_json_generic.sh
+++ make_json_generic.sh
@@ -88,7 +88,7 @@ elif [[ $deviceType == "I2C" ]]; then
   temperature=$(echo "scale=3; ($adjTmp * 0.1250) - $tmpNeg" | /usr/bin/bc | awk '{printf "%.2f\n", $0}')
   prev_temperature=$(jq -r .temperature /mnt/ramdisk/temperature.json)
     jq -c --null-input --arg ip "$ipaddress" --arg date "$datestring" \
-     --arg type "$deviceType" --arg name "$deviceName" --arg tmp "$temperature" --arg prev "$prev_temperature"\
+     --arg type "$deviceType" --arg name "$deviceName" --arg tmp "$temperature" --arg prev "$prev_temperature" \
      '{"name": $name, "IP": $ip, "date": $date, "temperature": $tmp, "prev": $prev, "type": $type }'
 exit 0
 
