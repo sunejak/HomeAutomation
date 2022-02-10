a96e1e14dc2d829139b40f039887caf7b2355643
Sunejak <sune.jakobsson@gmail.com>
2022-02-10 14:53:10 +0100

    Make plot more robust

--- devices/make_fireplace_plot.sh
+++ devices/make_fireplace_plot.sh
@@ -1,9 +1,26 @@
-
 #!/bin/bash
 #
 # get data from fireplace, and format for gnuplot
 #
-curl  -s 192.168.1.148/all_temperature.json | jq -c 'del(.name) | del(.IP) | del(.type) | del(.prev)' | cut -d'"' -f4,8 | tr '"' ' ' | cut -d' ' -f5,8 > plot.data
+fireplaceData=$(curl  -s 192.168.1.148/all_temperature.json )
+if [ $? -ne 0 ] ; then
+  echo "Could not access fireplace"
+  exit 1;
+fi
+
+# readingTimes=$(echo $fireplaceData | jq .date  | cut -d' ' -f4)
+# readingTemperature=$(echo $fireplaceData | jq  -r .temperature)
+
+declare -a arrayTimes=($(echo $fireplaceData | jq .date  | cut -d' ' -f4))
+declare -a arrayTemperature=($(echo $fireplaceData | jq  -r .temperature))
+arrayLength=${#arrayTimes[@]}
+
+rm plot.data
+
+for (( i=0; i<${arrayLength}; i++ ));
+do
+  echo ${arrayTimes[$i]} ${arrayTemperature[$i]} >> plot.data
+done
 #
 # create the plot
 #
