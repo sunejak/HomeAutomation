723adc6a03e501b105328d26135fc2acda9091a1
Sunejak <sune.jakobsson@gmail.com>
2022-02-22 22:16:09 +0100

    fixed names

--- www/html/find_biggest_pr_hour.sh
+++ www/html/find_biggest_pr_hour.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #
-location=driveway/02_Feb_2022/22/cam0_2022_Feb_22
+location="driveway/02_Feb_2022/22/cam0_2022_Feb_22"
 #
 echo "<!DOCTYPE html>"
 echo "<html><body>"
@@ -9,13 +9,10 @@ echo "<h1>$location</h1>"
 
 for j in {00..23}
 do
-
-for i in $(ls -S $location_$j* | head | sort);
-do
-  echo "<img src=\""$i"\" width=\"350\" height=\"350\">" ;
-
+ echo "<p>Hour: $j</p>"
+ for i in $(ls -S ${location}_$j* | head | sort);
+   do
+     echo "<img src=\""$i"\" width=\"350\" height=\"350\">"
+   done
 done
-  echo "<p>$j</p>" ;
-done
-
 echo "</body></html>"
