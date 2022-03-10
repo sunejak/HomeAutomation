19b98237b8cc42b54f3d926daaa8221644e74487
Sunejak <sune.jakobsson@gmail.com>
2022-03-10 12:54:31 +0100

    Converted to use updated sunrise/sunset format in JSON

--- devices/decide_OutdoorLight_withCurl.sh
+++ devices/decide_OutdoorLight_withCurl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# ./decide_OutdoorLights_withCurl.sh http://192.168.1.23/suntime.log "15:00 today" "01:00 tomorrow"
+# ./decide_OutdoorLights_withCurl.sh http://192.168.1.26/suntime.json "15:00 today" "01:00 tomorrow"
 #
 # 00:00 -> on until 1:00 -> off until 5:00 -> on until sunrise -> off until sunset -> on until 23:59
 #
@@ -14,30 +14,30 @@ if [ -z "$2" ]; then
   echo "Provide a begin time, when lights can turn on"
   exit 1
 fi
-offNightTime=$(date --date="$2" "+%s");    # 1:00
+offNightTime=$(date --date="$2" "+%s") # 1:00
 #
 if [ -z "$3" ]; then
   echo "Provide an end time, when lights must turn off"
   exit 1
 fi
-onMorningTime=$(date --date="$3" "+%s");  # 5:00
+onMorningTime=$(date --date="$3" "+%s") # 5:00
 #
 # fetch sunrise and sunset times, use RAM disk on pi.
 #
 response=$(curl -s ${URL})
-if [ $? -ne 0 ] ; then
+if [ $? -ne 0 ]; then
   echo "Could not access: ${URL}"
-  exit 1;
+  exit 1
 fi
 #
-today=$(echo $response | tr ' ' '\n' | grep $(date -I) )
-if [ $? -ne 0 ] ; then
+today=$(echo $response | tr ' ' '\n' | grep $(date -I))
+if [ $? -ne 0 ]; then
   echo "Could not find data for today"
-  exit 1;
+  exit 1
 fi
 
-sunsetTime=$( date -d $(echo $today | jq -r .Sunrise ) "+%s")
-sunriseTime=$( date -d $(echo $today | jq -r .Sunset ) "+%s")
+sunsetTime=$(date -d $(echo $today | jq -r .Sunset) "+%s")
+sunriseTime=$(date -d $(echo $today | jq -r .Sunrise) "+%s")
 #
 now=$(date "+%s")
 #
@@ -45,25 +45,25 @@ gpio mode 5 output
 turnOn=0
 
 # keep light on from midnight until given time
-if [ $now -lt $offNightTime ] ; then
-#  echo before 1:00
+if [ $now -lt $offNightTime ]; then
+  #  echo before 1:00
   turnOn=1
-  fi
+fi
 # turn on light in the morning until sunrise
-if [  $now -gt $onMorningTime ] && [ $now -lt $sunriseTime ] ; then
+if [ $now -gt $onMorningTime ] && [ $now -lt $sunriseTime ]; then
   turnOn=1
-#  echo after 5:00 before sunrise
-  fi
+  #  echo after 5:00 before sunrise
+fi
 # turn on light at sunset
-if [  $now -gt $sunsetTime ]  ; then
+if [ $now -gt $sunsetTime ]; then
   turnOn=1
-#  echo after sunset
-  fi
+  #  echo after sunset
+fi
 
-if [  $turnOn -eq 1 ] ; then
-      echo Lights on $now : $offNightTime $onMorningTime $sunriseTime $sunsetTime
-      gpio write 5 1
+if [ $turnOn -eq 1 ]; then
+  echo Lights on $now : $offNightTime $onMorningTime $sunriseTime $sunsetTime
+  gpio write 5 1
 else
-      echo Lights off $now : $offNightTime $onMorningTime $sunriseTime $sunsetTime
-      gpio write 5 0
+  echo Lights off $now : $offNightTime $onMorningTime $sunriseTime $sunsetTime
+  gpio write 5 0
 fi
--- devices/decide_onORoff_withCurl.sh
+++ devices/decide_onORoff_withCurl.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
 if [ -z "$1" ]; then
+  echo "Provide a URL and a delta time"
+  exit 1
+fi
+URL=$1
+if [ -z "$2" ]; then
   echo "Provide a time delta in hours"
   exit 1
 fi
-hours=$1
+hours=$2
 delta=$(echo "scale=0; (${hours} * 3600)/1" | bc )
-#
-if [ -z "$2" ]; then
-  echo "Provide a URL"
-  exit 1
-fi
-URL=$2
 #
 response=$(curl -s ${URL})
 if [ $? -ne 0 ] ; then
@@ -26,27 +25,29 @@ if [ -z "$today" ] ; then
 fi
 # pick out the maxHour
 #
+# curl -v http://192.168.1.26/electric.json | jq '. | select(.date=="2022-03-10") | .maxHour '
+#
 maxHour=$( echo $today | jq -r .maxHour)
 #
 # calculate when in seconds, compensate for time band
 #
-peaktime=$(echo "scale=0; ($maxHour * 3600 + 0.5 * 3600)/1" | bc )
+peakTime=$(echo "scale=0; ($maxHour * 3600 + 0.5 * 3600)/1" | bc )
 now=$(date "+%s")
 #
 # start of the day in seconds
 #
 morning=$(date -d $(date -I) "+%s")
 #
-starttime=$(( morning + peaktime - delta ))
-stoptime=$(( morning + peaktime + delta ))
+startTime=$(( morning + peakTime - delta ))
+stopTime=$(( morning + peakTime + delta ))
 #
-echo Time window from: $(date --date=@$starttime) to:  $(date --date=@$stoptime)
+echo Time window from: $(date --date=@$startTime) to:  $(date --date=@$stopTime)
 gpio mode 3 output
 #
-if [ $now -gt $starttime ] && [ $now -lt $stoptime ]; then
-  echo Inside window $(date) Time window from: $(date --date=@$starttime) to:  $(date --date=@$stoptime)
+if [ $now -gt $startTime ] && [ $now -lt $stopTime ]; then
+  echo Inside window $(date) Time window from: $(date --date=@$startTime) to:  $(date --date=@$stopTime)
   gpio write 3 1
   else
-  echo Outside window $(date) Time window from: $(date --date=@$starttime) to:  $(date --date=@$stoptime)
+  echo Outside window $(date) Time window from: $(date --date=@$startTime) to:  $(date --date=@$stopTime)
   gpio write 3 0
 fi
