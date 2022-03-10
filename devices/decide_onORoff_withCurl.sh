142b3742981dd8704651d860b1a734ff89255e1d
Sunejak <sune.jakobsson@gmail.com>
2022-03-10 12:26:18 +0100

    Converted to use updated sunrise/sunset format in JSON

--- devices/decide_onORoff_withCurl.sh
+++ devices/decide_onORoff_withCurl.sh
@@ -13,27 +13,34 @@ if [ -z "$2" ]; then
 fi
 URL=$2
 #
-data=$( curl -fsS -w '%{http_code}' ${URL} > /mnt/ramdisk/tmp.tmp )
+response=$(curl -s ${URL})
 if [ $? -ne 0 ] ; then
   echo "Could not access: ${URL}"
   exit 1;
 fi
-
-today=$(grep $(date -I) /mnt/ramdisk/tmp.tmp)
-if [ $? -ne 0 ] ; then
-  echo "Could not find data"
+#
+today=$(echo $response | sed "s/}/}\n/g" | grep $(date -I) )
+if [ -z "$today" ] ; then
+  echo "Could not find data for today ( $today )"
   exit 1;
 fi
-
-peaktime=$( echo $today | cut -f1 -d ' ')
-starttime=$(( peaktime - delta ))
-stoptime=$(( peaktime + delta ))
+# pick out the maxHour
 #
+maxHour=$( echo $today | jq -r .maxHour)
+#
+# calculate when in seconds, compensate for time band
+#
+peaktime=$(echo "scale=0; ($maxHour * 3600 + 0.5 * 3600)/1" | bc )
 now=$(date "+%s")
 #
-# date --date='@123'
+# start of the day in seconds
 #
-# echo Time window from: $(date --date=@$starttime) to:  $(date --date=@$stoptime)
+morning=$(date -d $(date -I) "+%s")
+#
+starttime=$(( morning + peaktime - delta ))
+stoptime=$(( morning + peaktime + delta ))
+#
+echo Time window from: $(date --date=@$starttime) to:  $(date --date=@$stoptime)
 gpio mode 3 output
 #
 if [ $now -gt $starttime ] && [ $now -lt $stoptime ]; then
