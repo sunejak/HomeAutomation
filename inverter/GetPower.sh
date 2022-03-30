dc1459b17ad4e0fc51747ca019cd840f3a60eda0
Sunejak <sune.jakobsson@gmail.com>
2022-03-30 20:58:30 +0200

    Power plot

--- inverter/GetPower.sh
+++ inverter/GetPower.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-#
-# 
 # The StatusCode Field is only reported as numerical value.
+# Inverter is with fixed IP address at 192.168.1.30
 #
 # 0 - 6 Startup 
 # 7     Running
@@ -10,7 +9,7 @@
 # 9     Boot loading
 # 10    Error 
 #
-resp=$(curl -s "http://192.168.1.174/solar_api/v1/GetInverterRealtimeData.cgi?scope=Device&DataCollection=CommonInverterData&DeviceId=1" )
+resp=$(curl -s "http://192.168.1.30/solar_api/v1/GetInverterRealtimeData.cgi?scope=Device&DataCollection=CommonInverterData&DeviceId=1" )
 if [ $? -ne 0 ] ; then
   echo "{ \"Body\" : { \"Data\" : { \"DAY_ENERGY\" : { \"Unit\" : \"Wh\", \"Value\" : -1.0 }, \"DeviceStatus\" : { \"StatusCode\" : -1 }}}, \"Head\" : {  \"Timestamp\" : \"2021-0-0\" } }"
   exit 1;
