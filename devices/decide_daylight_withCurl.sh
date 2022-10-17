d83f310e734baa777e5183e3b908f78b114a07f2
Sunejak <sune.jakobsson@gmail.com>
2022-10-17 12:31:37 +0200

    Updated daylight

--- devices/decide_daylight_withCurl.sh
+++ devices/decide_daylight_withCurl.sh
@@ -12,7 +12,7 @@ if [ $? -ne 0 ] ; then
   exit 1;
 fi
 #
-today=$(echo $response | tr ' ' '\n' | grep $(date -I) )
+today=$(echo $response | sed "s/}/}\n/g" | grep $(date -I) | tail -1 )
 if [ $? -ne 0 ] ; then
   echo "Could not find data for today"
   exit 1;
