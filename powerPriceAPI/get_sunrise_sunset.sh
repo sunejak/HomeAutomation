a3ed0ed7851cf0212b8a244218a9f3ff14405831
Sune Jakobsson <sune.jakobsson@telenor.no>
2023-04-09 12:13:48 +0200

    Yr changed the response

--- powerPriceAPI/get_sunrise_sunset.sh
+++ powerPriceAPI/get_sunrise_sunset.sh
@@ -16,18 +16,36 @@ if [ $? -ne 0 ] ; then
 fi
 # check number of entries
 #
-count=$(echo $celestialEvents | jq '.times | length')
-# echo $count
+count=$(echo $celestialEvents | jq '.events | length')
+echo $count
 #
 counter=0
+mySunrise=0
+mySunset=0
+
 while [ $counter -lt $count ]
 do
-  sunrise=$(echo $celestialEvents | jq -r .times[$counter].sun.sunrise )
-  sunset=$(echo $celestialEvents | jq -r .times[$counter].sun.sunset )
-  mySunrise=$(date -d $sunrise '+%s')
-  mySunset=$(date -d $sunset '+%s')
+  type=$(echo $celestialEvents | jq -r .events[$counter].type )
+  body=$(echo $celestialEvents | jq -r .events[$counter].body )
+#  echo $type
+  if [ $type == "Set" ] && [ $body == "Sun" ];
+  then
+    sunset=$(echo $celestialEvents | jq -r .events[$counter].time )
+    mySunset=$(date -d $sunset '+%s')
+  fi
 
-  jq -c --null-input --arg mYrise "$mySunrise" --arg rise "$sunrise" --arg mYset "$mySunset" --arg set "$sunset" \
-  '{"Rise": $mYrise, "Sunrise": $rise, "Set": $mYset, "Sunset": $set}'
+  if [ $type == "Rise" ] && [ $body == "Sun" ];
+  then
+    sunrise=$(echo $celestialEvents | jq -r .events[$counter].time )
+    mySunrise=$(date -d $sunrise '+%s')
+  fi
+
+  if [ $mySunset -ne 0 ] && [ $mySunrise -ne 0 ] ;
+  then
+    jq -c --null-input --arg mYrise "$mySunrise" --arg rise "$sunrise" --arg mYset "$mySunset" --arg set "$sunset" \
+    '{"Rise": $mYrise, "Sunrise": $rise, "Set": $mYset, "Sunset": $set}'
+    mySunset=0
+    mySunrise=0
+  fi
 ((counter ++))
 done
